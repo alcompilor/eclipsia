9f58656b9b802f6b02eb7e5c078dc23d9bb0345f
Ahmed Abbasi <contact@ahmed.dev>
2024-05-01 00:09:31 +0200

    Use `loadChain.js` and `persistChain.js` to manage eclipsia chain storage

--- rpc.js
+++ rpc.js
@@ -1,9 +1,12 @@
 import express from "express";
 import helmet from "helmet";
 import { logger } from "./middlewares/logger.js";
+import { loadChain } from "./utils/loadChain.js";
+import { persistChain } from "./utils/persistChain.js";
 
 const PORT = 7185;
 const rpc = express();
+let eclipsia;
 
 rpc.use(express.json());
 rpc.use(express.urlencoded({ extended: true }));
@@ -11,5 +14,11 @@ rpc.use(helmet());
 rpc.use(logger);
 
 rpc.listen(PORT, (err) => {
-    err ? console.log("Failed to run RPC") : console.log("RPC is up and running..\n")
+    err ? console.log("Failed to run RPC") : console.log("RPC is up and running..\n");
+    eclipsia = loadChain();
+});
+
+process.on("SIGINT", () => {
+    persistChain(eclipsia);
+    process.exit(0);
 });
