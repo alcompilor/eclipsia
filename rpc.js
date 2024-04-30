4e7dded8ef68b1b1848377ffad5a3130fb3c02c7
Ahmed Abbasi <contact@ahmed.dev>
2024-04-30 17:20:05 +0200

    Configure logger as middleware in RPC module

--- rpc.js
+++ rpc.js
@@ -1,5 +1,6 @@
 import express from "express";
 import helmet from "helmet";
+import { logger } from "./middlewares/logger.js";
 
 const PORT = 7185;
 const rpc = express();
@@ -7,7 +8,8 @@ const rpc = express();
 rpc.use(express.json());
 rpc.use(express.urlencoded({ extended: true }));
 rpc.use(helmet());
+rpc.use(logger);
 
 rpc.listen(PORT, (err) => {
-    err ? console.log("Failed to run RPC") : console.log("RPC is up and running..")
+    err ? console.log("Failed to run RPC") : console.log("RPC is up and running..\n")
 });
